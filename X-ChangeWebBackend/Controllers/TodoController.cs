cd122541615f89c6429c8e933750cb3ad28bd7ba
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2026-02-02 15:03:53 +0200

    Create new

---
+++ X-ChangeWebBackend/Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace X_ChangeWebBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            var todos = new[]
+            {
+                new { Id = 1, Task = "Buy groceries", IsCompleted = false },
+                new { Id = 2, Task = "Walk the dog", IsCompleted = true },
+                new { Id = 3, Task = "Finish project", IsCompleted = false }
+            };
+            return Ok(todos);
+        }
+    }
+}
