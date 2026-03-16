4d016cfcbcdf02811898210095e5bc606a69df6d
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2026-03-16 08:42:16 +0200

    .

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431535be2a42e8350a6238ef5b8797d86f7f8a36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6f481cda6dca115d602d8883ec980e9a4fb456")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
