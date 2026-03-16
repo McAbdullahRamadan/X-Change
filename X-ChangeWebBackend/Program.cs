98de18723723dd076fce6a960373a35b77b2c2de
Abdullah Ramadan Ali <abdullah.ramadanali@gmail.com>
2026-03-16 09:27:19 +0200

    RoutePrefix

--- X-ChangeWebBackend/Program.cs
+++ X-ChangeWebBackend/Program.cs
@@ -76,6 +76,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "X-Change API v1");
+        options.RoutePrefix = string.Empty;
     });
 }
 
